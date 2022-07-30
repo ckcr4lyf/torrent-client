ab940e38895896d8fb62bfc82b29f9bc1942c3fc
Raghu Saxena <poiasdpoiasd@live.com>
2022-07-30 15:15:20 +0800

    Torrent via fixtures

--- __tests__/main.ts
+++ __tests__/main.ts
@@ -1,10 +1,16 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
 import test from 'ava';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import { parseTorrent } from '../src/torrent.js'
 
 const fn = () => 'foo';
 
 test('fn() returns foo', t => {
+    const archTorrent = fs.readFileSync(path.join(__dirname, './fixtures/archlinux-2022.07.01-x86_64.iso.torrent'));
 	t.is(fn(), 'foo');
-    parseTorrent(Buffer.from("XD"));
+    parseTorrent(archTorrent);
 });
